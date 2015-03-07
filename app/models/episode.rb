41ce2cc6aef4d38b816cbe8c833f4b1ac9f79d78
Ash Harris <ash.compsci@gmail.com>
2015-03-07 12:49:39 -0600

    fix podtrac check when creating new episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,7 +2,7 @@ class Episode < ActiveRecord::Base
   belongs_to :feed
 
   def url
-    if feed.uses_podtrac
+    if feed && feed.uses_podtrac
       "http://www.podtrac.com/pts/redirect.mp3/#{read_attribute(:url)}"
     else
       read_attribute(:url)
