1c87196d88e60dacd7eef2bb4bf191f3cc5203d3
Ash Harris <ash.compsci@gmail.com>
2015-03-09 21:41:48 -0500

    move FTP details to env, tidy up a bit, fix mp3 file types

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -5,54 +5,51 @@ class Episode < ActiveRecord::Base
     # Choose the FTP storage backend
     :storage => :ftp,
 
-
-    #:path => ftp_path+"/:filename",
-    #:url => feed.ftp_folder_url+"/:filename",
-    #:path => "/:filename",
-    #:url => "www.codeandcountry.com/uploadtest/:filename",
-
-
     :path => lambda { |attachment| attachment.instance.ftp_path },
     :url => lambda { |attachment| attachment.instance.ftp_url },
 
     # The list of FTP servers to use
     :ftp_servers => [
       {
-        :host     => "ftp.codeandcountry.com",
-        :user     => "test@flashharris.com",
-        :password => "uploadtest"
+        :host     => ENV['FTP_HOST'],
+        :user     => ENV['FTP_USER'],
+        :password => ENV['FTP_PASSWORD']
       }
     ]
   }
 
-  validates_attachment :mp3, content_type: { content_type: ["audio/mpeg3", "audio/x-mpeg-3", "image/png"] } 
+  validates_attachment :mp3, content_type: { content_type: ["audio/mpeg3", "audio/x-mpeg-3", 'audio/mp3', 'application/x-mp3', "image/png"] } 
 
   def url
     if feed.uses_podtrac
-      "http://www.podtrac.com/pts/redirect.mp3/#{feed.ftp_folder_url}#{mp3.path}"
+      "http://www.podtrac.com/pts/redirect.mp3/"+file_url
     else
+      file_url
+    end
+  end
+
+  def file_url
+    if mp3
       "#{feed.ftp_folder_url}#{mp3.path}"
+    else
+      read_attribute(url)
     end
   end
 
   def ftp_path
     if feed 
-      f = feed.ftp_folder_path+"/:filename"
+      feed.ftp_folder_path+"/:filename"
     else
-      f = "/:filename"
+      ENV['FTP_DEFAULT_PATH']+"/:filename"
     end
-    puts f
-    f
   end
 
   def ftp_url
     if feed 
-      f = feed.ftp_folder_url+"/:filename"
+      feed.ftp_folder_url+"/:filename"
     else
-      f = "www.default.com/:filename"
+      ENV['FTP_DEFAULT_URL']+"/:filename"
     end
-    puts f
-    f
   end
 
 end
